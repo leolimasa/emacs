812429fe0272f42a82d389d31c75ddba00ac939a
lsa <leo@leo-sa.com>
2015-09-09 21:31:15 -0500

    Added showing bookmark on startup

--- leo.el
+++ leo.el
@@ -9,3 +9,15 @@
 (load "leo-keys.el")
 (load "leo-custom-funs.el")
 (load "leo-haskell.el")
+
+; Show bookmarks when it's all done
+(defun show-bookmarks ()
+	(setq inhibit-splash-screen t)
+	(require 'bookmark)
+	;(select-window-2)
+	(split-window-below)
+	(bookmark-bmenu-list)
+	(switch-to-buffer "*Bookmark List*")
+)
+(add-hook 'after-init-hook
+	  '(lambda () (show-bookmarks)))
