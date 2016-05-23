0b166e42c49b51ad812fb80b5b26b6c687b50946
Leo <leo@leo-sa.com>
2016-05-23 02:20:44 -0500

    Updated elm

--- leo-elm.el
+++ leo-elm.el
@@ -1,8 +1,11 @@
-(require 'flycheck)
-(require 'flycheck-elm)
-(require 'company)
-(require 'elm-mode)
 
 ; flycheck
-(with-eval-after-load 'flycheck
-      '(add-hook 'flycheck-mode-hook #'flycheck-elm-setup))
+(add-hook 'flycheck-mode-hook 'flycheck-elm-setup)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+; Company autocompletion
+; Needs npm install elm-oracle
+; Triggered by C-c C-t
+(with-eval-after-load 'company
+  (add-to-list 'company-backends 'company-elm))
+(add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
