6b50cb2e17f6b2412e5af410476bb55d5b162842
Leonardo Sa <leonardo.sa@cerner.com>
2016-05-23 13:21:40 -0500

    Added tern support and updated the package list

--- leo-js.el
+++ leo-js.el
@@ -1,5 +1,20 @@
+; Fixes autocomplete
+(add-hook 'js2-mode-hook 'ac-js2-setup-auto-complete-mode)
+
 (add-hook 'js2-mode-hook
           (defun my-js2-mode-setup ()
             (flycheck-mode t)
             (when (executable-find "eslint")
               (flycheck-select-checker 'javascript-eslint))))
+
+
+; Enables tern
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+; npm install tern -g
+; npm install eslint -g 
